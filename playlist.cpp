66cb8aac6c17037ae3c0d5386092b5f655f7ca80
IlhamKurniawan46 <milhamk059@gmail.com>
2025-05-24 21:11:32 +0700

    Tambah input lagu, cari lagu

--- playlist.cpp
+++ playlist.cpp
@@ -24,6 +24,28 @@ struct Playlist {
 
 Playlist* headPlaylist = nullptr;
 
+// === Cari Playlist ===
+Playlist* cariPlaylist(const char* nama) {
+    Playlist* temp = headPlaylist;
+    while (temp) {
+        if (strcmp(temp->nama, nama) == 0)
+            return temp;
+        temp = temp->next;
+    }
+    return nullptr;
+}
+
+// === Cari Lagu ===
+Node* cariLaguNode(Playlist* pl, const char* judul) {
+    Node* temp = pl->head;
+    while (temp) {
+        if (strcmp(temp->data.judul, judul) == 0)
+            return temp;
+        temp = temp->next;
+    }
+    return nullptr;
+}
+
 // Menu 1 Buat Playlist
 void buatPlaylist() {
     Playlist* baru = new Playlist;
@@ -40,6 +62,63 @@ void buatPlaylist() {
         temp->next = baru;
     }
     cout << "Playlist berhasil dibuat!\n";
+}
+
+// Menu 2 Tambah Lagu
+void tambahLagu(const char* namaPlaylist) {
+    Playlist* pl = cariPlaylist(namaPlaylist);
+    if (!pl) {
+        cout << "Playlist tidak ditemukan!\n";
+        return;
+    }
+
+    int jumlah;
+    cout << "Jumlah lagu yang ingin ditambahkan: ";
+    cin >> jumlah;
+    cin.ignore();
+
+    for (int i = 0; i < jumlah; i++) {
+        Node* baru = new Node;
+        cout << "Judul lagu: "; cin.getline(baru->data.judul, 50);
+        cout << "Penyanyi: "; cin.getline(baru->data.penyanyi, 50);
+        cout << "Genre: "; cin.getline(baru->data.genre, 50);
+        char fav;
+        cout << "Favorit? (y/n): "; cin >> fav; cin.ignore();
+        baru->data.favorit = (fav == 'y' || fav == 'Y');
+        baru->prev = baru->next = nullptr;
+
+        if (!pl->head)
+            pl->head = pl->tail = baru;
+        else {
+            pl->tail->next = baru;
+            baru->prev = pl->tail;
+            pl->tail = baru;
+        }
+    }
+}
+
+// === 3. Cari Lagu ===
+void cariLagu(const char* namaPlaylist) {
+    Playlist* pl = cariPlaylist(namaPlaylist);
+    if (!pl) {
+        cout << "Playlist tidak ditemukan!\n";
+        return;
+    }
+
+    char judul[50];
+    cout << "Judul lagu dicari: ";
+    cin.getline(judul, 50);
+
+    Node* found = cariLaguNode(pl, judul);
+    if (found) {
+        cout << "Lagu ditemukan!\n";
+        cout << "Judul: " << found->data.judul << "\n";
+        cout << "Penyanyi: " << found->data.penyanyi << "\n";
+        cout << "Genre: " << found->data.genre << "\n";
+        cout << "Favorit: " << (found->data.favorit ? "Ya" : "Tidak") << "\n";
+    } else {
+        cout << "Lagu tidak ditemukan.\n";
+    }
 }
 
 int main(){
